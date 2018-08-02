60c7a2cace5d105747e9680ddb3333fd46a504da
imtiaz <programmer.imtiaz@gmail.com>
2018-08-02 09:04:50 +0600

    ADD- ButterKnife Added into the project

--- app/src/main/java/com/example/dipto/simplemvvmproject/MainActivity.java
+++ app/src/main/java/com/example/dipto/simplemvvmproject/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.dipto.simplemvvmproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_username)
+    EditText etUsername;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.pb_loader)
+    ProgressBar pbLoader;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.tv_phone)
+    TextView tvPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //TODO
     }
 }
