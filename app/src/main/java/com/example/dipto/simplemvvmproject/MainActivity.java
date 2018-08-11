ed03a7a95156a146b476ba4f5262a79288b91e8e
imtiaz <programmer.imtiaz@gmail.com>
2018-08-11 09:03:21 +0600

    ADD- LoginViewModel Instance created

--- app/src/main/java/com/example/dipto/simplemvvmproject/MainActivity.java
+++ app/src/main/java/com/example/dipto/simplemvvmproject/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.dipto.simplemvvmproject;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.example.dipto.simplemvvmproject.viewmodel.LoginViewModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,11 +29,13 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_phone)
     TextView tvPhone;
 
+    LoginViewModel loginViewModel ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
     }
 }
